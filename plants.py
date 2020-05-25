c07bdb40dfa599eac63e1b4fe3a23325ea902a26
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2020-05-25 14:36:55 +0100

    add checkuser route

--- plants.py
+++ plants.py
@@ -18,7 +18,10 @@ from sqlalchemy.orm import sessionmaker
 from database import Base, Plant
 
 #Connect to Database and create database session
-engine = create_engine('sqlite:///plants.db')
+engine = create_engine(
+                'sqlite:///plants.db',
+                connect_args={'check_same_thread': False}
+                )
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -35,41 +38,53 @@ def login():
 # home page showing all plants for that user
 @app.route("/home")
 def home():
-    # some logic for when user has no plants
-    user_plants_info = session.query(Plant).filter_by(user_email).all()
-    return render_template('home.html', user_plants_info = user_plants_info)
+    user_email = request.args.get('email')
 
-# check if current user exists
+
+
+    # what if url typed in by hand with invalid email - in database? valid email?
+    user_plants_info = session.query(Plant).filter_by(email = user_email).all()
+    return render_template('home.html', user_plants_info = user_plants_info)
+            
+#check if current user exists
 @app.route("/checkuser")
 def checkuser():
+    print("hello")
     user_email = request.args.get('email')
     # show message if user doesnt exist
-    if email: 
-        existing_user = session.query(Plant).filter_by(user_email = email).first()
-        if existing_user == false:
-            return make_response(f'{email} doesnt exist!')
-        else
-            return redirect(url_for("home"))
-
-@app.route("/newuser")
-def newuser():
-    user_email = request.args.get('email')
-    # show message if user doesnt exist
-    if email: 
-        existing_user = session.query(Plant).filter_by(user_email = email).first()
+    print(user_email)
+    if user_email: 
+        existing_user = session.query(Plant).filter_by(email = user_email).first()
         if existing_user:
-            return make_response(f'{email} already exists.')
-        else
-            return redirect(url_for("home"))
+            return 'true'
+        else:
+            return 'false'
+    # what if not user_email?
 
 
-# create new plant
-@app.route('/new_plant', methods=['GET', 'POST'])
-def new_plant():
-    if request.method == 'POST':
 
-        new_plant = Plant(email=request.form["email"], plant=request.form["plantname"], comments=request.form["comments"],
-                        interval=request.form["interval"], frequency=request.form["frequency"], date=request.form["start date"])
+
+# check for valid email
+
+# @app.route("/newuser")
+# def newuser():
+#     user_email = request.args.get('email')
+#     # show message if user doesnt exist
+#     if email: 
+#         existing_user = session.query(Plant).filter_by(user_email = email).first()
+#         if existing_user:
+#             return make_response(f'{email} already exists.')
+#         else
+#             return redirect(url_for("home"))
+
+
+# # create new plant
+# @app.route('/new_plant', methods=['GET', 'POST'])
+# def new_plant():
+#     if request.method == 'POST':
+
+#         new_plant = Plant(email=request.form["email"], plant=request.form["plantname"], comments=request.form["comments"],
+#                         interval=request.form["interval"], frequency=request.form["frequency"], date=request.form["start date"])
 
 
 # add event to google calendar
